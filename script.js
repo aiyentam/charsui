a79b29fb80838f9e99c01d2fe69d2b11010fc6bb
Ai Yen Tam <tam.aiyen@gmail.com>
2018-02-26 14:17:19 -0500

    new personal page

---
+++ script.js
@@ -0,0 +1,22 @@
+function openNav() {
+  document.getElementById("nav").style.height = "50%";
+}
+
+function closeNav() {
+  document.getElementById("nav").style.height = "0%";
+}
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         document.getElementsByClassName("topbtn").style.display = "block";
+//     } else {
+//         document.getElementsByClassName("topbtn").style.display = "none";
+//     }
+// }
+
+// function topFunction() {
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+// }
